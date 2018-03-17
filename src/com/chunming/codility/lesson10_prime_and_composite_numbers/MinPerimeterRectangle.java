b0b468f1765d3eea9fb76fa4f3c8af7308409687
Chun-Ming <sokunmin@gmail.com>
2018-03-17 17:54:15 +0800

    Complete MinPerimeterRectangle of lesson 10.

--- src/com/chunming/codility/lesson10_prime_and_composite_numbers/MinPerimeterRectangle.java
+++ src/com/chunming/codility/lesson10_prime_and_composite_numbers/MinPerimeterRectangle.java
@@ -14,19 +14,54 @@ package com.chunming.codility.lesson10_prime_and_composite_numbers;
   expected worst-case time complexity is O(sqrt(N));
   expected worst-case space complexity is O(1).
 
+  +-------------------+
+  | Task Score:  100% |
+  | Correctness: 100% |
+  | Performance: 100% |
+  +-------------------+
  */
 
 import com.chunming.Callback;
 
 public class MinPerimeterRectangle implements Callback {
 
-    public int solution(int[] A) {
-        return -1;
+    public int solution(int N) {
+        int minA = 0, minB = 0;
+        int sqrtN = (int) Math.sqrt(N);
+        // the length of A and B shoud be almost the same, just like a square.
+        for (int i = sqrtN; i >= 1; i--) {
+            if (N % i == 0) {
+                minA = i;
+                minB = N / minA;
+                break;
+            }
+        }
+
+        return 2 * (minA + minB);
+    }
+
+    // contributed by charles_wangkai
+    public int solution2(int N) {
+        int minPerimeter = Integer.MAX_VALUE;
+        for (int i = 1; i * i <= N; i++) {
+            if (N % i == 0) {
+                minPerimeter = Math.min(minPerimeter, 2 * (i + N / i));
+            }
+        }
+        return minPerimeter;
     }
 
     @Override
     public void test() {
-        int[] A = {};
-        System.out.println("" + solution(A));
+        System.out.println("result: " + solution(30));
+        System.out.println("result: " + solution(35));
+        System.out.println("result: " + solution(36));
+        System.out.println("result: " + solution(37));
+        System.out.println("-------------");
+        System.out.println("result: " + solution2(30));
+        System.out.println("result: " + solution2(35));
+        System.out.println("result: " + solution2(36));
+        System.out.println("result: " + solution2(37));
+
     }
 }
